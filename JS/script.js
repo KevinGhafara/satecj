72ed47f7c048e2ebcd1370fe21644222ad43dfe0
KevinGhafara <kevinghafara12@gmail.com>
2024-04-21 23:01:56 +0700

    menambah ui search box

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,30 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const NavbarNav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   NavbarNav.classList.toggle("active");
 };
 
-// Klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Klik diluar element
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
+// Toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !NavbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !NavbarNav.contains(e.target)) {
     NavbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
